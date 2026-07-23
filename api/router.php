e36ee5aad9ed86c9ba1434ef380f8c9698a7833f
zamanalisandhu <techiesline@gmail.com>
2026-07-23 15:13:08 +0500

    Enable error reporting in router.php for Vercel debugging

--- api/router.php
+++ api/router.php
@@ -3,6 +3,10 @@
  * RDT Video Downloader - Development Router for PHP Built-in Server
  */
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 // Normalize path (strip trailing slashes)
