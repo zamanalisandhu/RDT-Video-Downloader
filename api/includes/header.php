7a73fb978334bf14d86a4b3ca03d9a08bd60b36f
zamanalisandhu <techiesline@gmail.com>
2026-07-24 11:04:40 +0500

    Add Google Analytics tag (gtag.js) to header template

--- api/includes/header.php
+++ api/includes/header.php
@@ -86,6 +86,15 @@ function is_active_route($href) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-JN1CF5G7T8"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-JN1CF5G7T8');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
